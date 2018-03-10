1b6b3b2757d8ce71845154b48ecf241d8f0a3b58
Yoshihiro Kimura <kimura.yoshihiro@tsunolab.net>
2018-03-10 12:56:51 +0900

    webpacker導入

--- app/frontend/javascripts/application.js
+++ app/frontend/javascripts/application.js
@@ -7,4 +7,9 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+import 'stylesheets/application';
+import Rails from 'rails-ujs';
+
+require.context('images', true, /\.(png|jpg|jpeg|svg)$/)
+
+Rails.start();
