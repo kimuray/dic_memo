9868fb3fa7d54e5e9d704bd4e6e86c76ca48580b
芝田大樹 <shibatadaiki@shibatadaikinoMacBook-Air.local>
2018-03-16 08:05:19 +0900

    assignのコントローラ作成 #5

---
+++ app/controllers/assigns_controller.rb
@@ -0,0 +1,17 @@
+class AssignsController < ApplicationController
+  def create
+    @informations = User.assigned!(assign_params, assign_user_params)
+    @project = Project.find(assign_params[:project_id])
+    @assign = @project.assigns.new
+    render 'projects/show'
+  end
+
+  private
+    def assign_params
+      params.require(:assign).permit(:project_id)
+    end
+
+    def assign_user_params
+      params.require(:user).permit(:email)
+    end
+end
