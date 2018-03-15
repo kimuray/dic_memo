88f5e95607ae729931c121c14b11cacd83bc33e9
芝田大樹 <shibatadaiki@shibatadaikinoMacBook-Air.local>
2018-03-16 00:47:25 +0900

    user編集用のコントローラの記述追記

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:edit, :update]
+  before_action :set_user, only: [:edit, :update]
+
   def dashboard
   end
+
+  def edit
+  end
+
+  def update
+    if @user.update(users_params)
+      redirect_to edit_user_path(@user), notice: 'User情報の更新が終わりました'
+    else
+      render 'edit'
+    end
+  end
+
+  private
+    def users_params
+      params.require(:user).permit(:name, :email, :image, :password)
+    end
+
+    def set_user
+      @user = User.find(params[:id])
+    end
 end
