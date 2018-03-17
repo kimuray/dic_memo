56b11410d9e3fd8a1fdc2553743f7965748bb505
芝田大樹 <shibatadaiki@shibatadaikinoMacBook-Air.local>
2018-03-17 11:33:12 +0900

    dashboardもログイン後に見れるように修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only: [:edit, :update]
+  before_action :authenticate_user!
   before_action :set_user, only: [:edit, :update]
 
   def dashboard
