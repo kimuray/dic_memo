b625e4f5e3e4dbe83e248daf74d61cf8ebc788af
芝田大樹 <shibatadaiki@shibatadaikinoMacBook-Air.local>
2018-03-30 07:42:14 +0900

    ラインモデルとその周辺のアソシエーション定義

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,7 @@
 class Project < ApplicationRecord
   belongs_to :user
+
   has_many :assigns, dependent: :destroy
   has_many :assigned_users, through: :assigns, source: :user
+  has_many :lines, dependent: :destroy
 end
