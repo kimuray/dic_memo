53926ccc3286361db53a4a1b0276100f377b454c
shibatadaiki <shibatadaiki92@gmail.com>
2018-04-09 02:46:42 +0900

    lines_controller.rbにeditやdestroyなどの基本機能を追記

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -1,5 +1,6 @@
 class LinesController < ApplicationController
   before_action :set_line, only: [:edit, :update, :destroy]
+  before_action :set_project, only: [:edit, :update, :destroy]
 
   def new
     @project = Project.find(params[:project_id].to_i)
@@ -7,29 +8,31 @@ class LinesController < ApplicationController
   end
 
   def edit
-
   end
 
   def create
     @project = Project.find(params[:project_id].to_i)
     @line = current_user.lines.build(line_params)
-    unless @project.lines.first.nil?
-      @line.position = @project.lines.order('position DESC').first.position + 1
-    else
-      @line.position = 1
-    end
-    @line.save
+    @project.lines.first.nil? ? @line.position = 1 : @line.position = @project.lines.order('position ASC').last.position + 1
+    @line.save ? (redirect_to @project, notice: I18n.t("notice.lines.create")) : (render :new)
   end
 
   def update
+    @line.update(line_params) ? (redirect_to @project, notice: I18n.t("notice.lines.update")) : (render :edit)
   end
 
   def destroy
+    @line.destroy
+    redirect_to @project, notice: I18n.t("notice.lines.destroy")
   end
 
   private
   def set_line
     @line = Line.find(params[:id])
+  end
+
+  def set_project
+    @project = @line.project
   end
 
   def line_params
